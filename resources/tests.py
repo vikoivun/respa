ce66b466d7a93540e6d8ec3a5dd602ad5e70f5fb
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2015-07-06 18:27:13 +0300

    Start of a test case for new Period and Date handling logic
     NOTE: does not work yet, since more stringent validators
     require actual data objects, not the preliminary versions
     model's save method's provide when used directly with
     model's create method

--- resources/tests.py
+++ resources/tests.py
@@ -53,3 +53,41 @@ class ReservationTestCase(TestCase):
         begin = arrow.get('2015-06-01T16:00:00+03:00')
         Reservation.objects.create(resource=r1a, begin=begin,
                                    end=begin + datetime.timedelta(hours=2))
+
+class DayTestCase(TestCase):
+    """
+    # Test case for day handler
+
+    Creates a week of regular period with open hours and two day closed exceptional period
+
+    Tests that day creation works as it should
+    """
+
+    def setUp(self):
+        u1 = Unit.objects.create(name='Unit 1', id='unit_1')
+        u2 = Unit.objects.create(name='Unit 2', id='unit_2')
+        rt = ResourceType.objects.create(name='Type 1', id='type_1', main_type='space')
+        Resource.objects.create(name='Resource 1a', id='r1a', unit=u1, type=rt)
+        Resource.objects.create(name='Resource 1b', id='r1b', unit=u1, type=rt)
+        Resource.objects.create(name='Resource 2a', id='r2a', unit=u2, type=rt)
+        Resource.objects.create(name='Resource 2b', id='r2b', unit=u2, type=rt)
+
+        # Regular hours for one week
+        p1 = Period.objects.create(start='2015-08-03', end='2015-08-09', unit=u1, name='regular hours')
+        Day.objects.create(period=p1, weekday=0, opens='08:00', closes='18:00')
+        Day.objects.create(period=p1, weekday=1, opens='08:00', closes='18:00')
+        Day.objects.create(period=p1, weekday=2, opens='08:00', closes='18:00')
+        Day.objects.create(period=p1, weekday=3, opens='08:00', closes='18:00')
+        Day.objects.create(period=p1, weekday=4, opens='08:00', closes='18:00')
+        Day.objects.create(period=p1, weekday=5, opens='12:00', closes='16:00')
+        Day.objects.create(period=p1, weekday=6, opens='12:00', closes='14:00')
+
+        # Two shorter days as exception
+        exp1 = Period.objects.create(start='2015-08-06', end='2015-08-07', unit=u1,
+                                     name='exceptionally short days', exception=True, parent=p1)
+        Day.objects.create(period=exp1, weekday=3, opens='12:00', closes='16:00')
+        Day.objects.create(period=exp1, weekday=4, opens='12:00', closes='16:00')
+
+        # Weekend is closed as an exception
+        exp2 = Period.objects.create(start='2015-08-08', end='2015-08-09', unit=u1, name='weekend is closed',
+                                     closed=True, exception=True, parent=p1)
